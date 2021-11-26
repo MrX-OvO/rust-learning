d7b0885fe9a82e79392bc33ec18a80b7d6c874a6
MrXOvO <1176471624@qq.com>
2021-11-26 16:45:56 +0800

    second time, ownership_function, add func givenowners() and tunc take_and_give_back_ownership()

--- ownership_function/src/main.rs
+++ ownership_function/src/main.rs
@@ -6,6 +6,15 @@ fn main() {
     let x = 1024;
     make_copy(x);
     println!("after called func make_copy, x={}", x);
+
+    let required_str = givenowners();
+    println!(
+        "after called func givenowners, requiredStr={}",
+        required_str
+    );
+
+    let s = take_and_give_back_ownership(required_str);
+    println!("after called func take_and_give_back_ownership, s={}", s);
 }
 
 fn takeownership(owners: String) {
@@ -14,4 +23,13 @@ fn takeownership(owners: String) {
 
 fn make_copy(somes: i32) {
     println!("somes: {:?}", somes);
+}
+
+fn givenowners() -> String {
+    let s = String::from("Hello");
+    s
+}
+
+fn take_and_give_back_ownership(owners: String) -> String {
+    owners
 }

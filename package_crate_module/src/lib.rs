25880803b0eb13df0674193dc4e4c17b083fe49e
MrXOvO <1176471624@qq.com>
2021-11-28 15:11:56 +0800

    second commit

--- package_crate_module/src/lib.rs
+++ package_crate_module/src/lib.rs
@@ -1,12 +1,51 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
         fn seat_at_table() {}
     }
 
-    mod serving {
+    pub mod serving {
         fn take_order() {}
-        fn serve_order() {}
+        pub fn serve_order() {}
         fn take_payment() {}
     }
+}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::front_of_house::serving::serve_order();
+    }
+    fn cook_order() {}
+
+    pub struct Breakfast {
+        // struct的字段默认私有，通过设置pub关键字变为共有
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    pub enum Appetizer {
+        // 都是公共的，包括enum和enum的变体
+        Soup,
+        Salad,
+    }
+}
+
+pub fn eat_at_restaurant() {
+    crate::front_of_house::hosting::add_to_waitlist(); // error, module `hosting` is private, function `add_to_waitlist` is private
+    front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+    // meal.seasonal_fruit = String::from("blueberries"); // field `seasonal_fruit` of struct `Breakfast` is private
 }

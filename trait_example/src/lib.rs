26b2feacbe009448bdee0ba31f83722d77408011
MrXOvO <1176471624@qq.com>
2021-12-11 17:30:12 +0800

    second commit, defaut trait

--- trait_example/src/lib.rs
+++ trait_example/src/lib.rs
@@ -1,5 +1,10 @@
 pub trait Summary {
-    fn summarize(&self) -> String;
+    // fn summarize(&self) -> String;
+    fn summarize(&self) -> String {
+        format!("(Read more from {})", self.summarize_author())
+    }
+
+    fn summarize_author(&self) -> String;
 }
 
 pub struct NewsArticle {
@@ -13,6 +18,9 @@ impl Summary for NewsArticle {
     fn summarize(&self) -> String {
         format!("{}, by {} ({})", self.headline, self.author, self.location)
     }
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.author)
+    }
 }
 
 pub struct Tweet {
@@ -25,5 +33,8 @@ pub struct Tweet {
 impl Summary for Tweet {
     fn summarize(&self) -> String {
         format!("{}: {}", self.username, self.content)
+    }
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
     }
 }

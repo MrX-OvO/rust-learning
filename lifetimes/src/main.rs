3fe59e323ca610eb45f9129bd502bdc5ebbc5c65
MrX-OvO <1176471624@qq.com>
2022-02-11 19:30:20 +0800

    struct 定义中生命周期标注(引用)

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -1,4 +1,10 @@
+// 3.结构体定义中生命周期标注（引用）
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
 fn main() {
+    //1.
     /*  let r;
     {
         let x = 5;
@@ -12,7 +18,7 @@ fn main() {
 
     println!("The longest string is {}", result);
 
-    // 标注生命周期 'a
+    // 2.标注生命周期 'a
     fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
         if x.len() > y.len() {
             x
@@ -20,4 +26,11 @@ fn main() {
             y
         }
     }
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
 }

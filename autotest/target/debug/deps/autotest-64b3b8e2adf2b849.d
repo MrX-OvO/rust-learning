aa544db795deaddc080051d3fbfa65a1cb37fd1e
MrX-OvO <1176471624@qq.com>
2022-02-11 19:55:14 +0800

    first autotest

---
+++ autotest/target/debug/deps/autotest-64b3b8e2adf2b849.d
@@ -0,0 +1,7 @@
+/home/mrxovo/Programs/rust/rust-learning/autotest/target/debug/deps/autotest-64b3b8e2adf2b849.rmeta: src/lib.rs
+
+/home/mrxovo/Programs/rust/rust-learning/autotest/target/debug/deps/libautotest-64b3b8e2adf2b849.rlib: src/lib.rs
+
+/home/mrxovo/Programs/rust/rust-learning/autotest/target/debug/deps/autotest-64b3b8e2adf2b849.d: src/lib.rs
+
+src/lib.rs:
---
+++ autotest/target/debug/deps/autotest-69bb04f8546da58f.d
@@ -0,0 +1,5 @@
+/home/mrxovo/Programs/rust/rust-learning/autotest/target/debug/deps/autotest-69bb04f8546da58f: src/lib.rs
+
+/home/mrxovo/Programs/rust/rust-learning/autotest/target/debug/deps/autotest-69bb04f8546da58f.d: src/lib.rs
+
+src/lib.rs:

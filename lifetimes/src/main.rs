b35072b1c3bfc37f9347a2922522836847ebfcf9
MrX-OvO <1176471624@qq.com>
2022-02-11 19:45:57 +0800

    方法中的生命周期

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -1,6 +1,26 @@
+// 2.标注生命周期 'a
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
 // 3.结构体定义中生命周期标注（引用）
 struct ImportantExcerpt<'a> {
     part: &'a str,
+}
+
+// 4.方法定义中的生命周期
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        1
+    }
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
+    }
 }
 
 fn main() {
@@ -18,19 +38,13 @@ fn main() {
 
     println!("The longest string is {}", result);
 
-    // 2.标注生命周期 'a
-    fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
-        if x.len() > y.len() {
-            x
-        } else {
-            y
-        }
-    }
-
     let novel = String::from("Call me Ishmael. Some years ago...");
     let first_sentence = novel.split('.').next().expect("Could not find a '.'");
 
     let i = ImportantExcerpt {
         part: first_sentence,
     };
+
+    // 生命周期的省略
+    // 静态生命周期 'static ：整个程序的持续时间
 }

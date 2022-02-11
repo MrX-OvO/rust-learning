3a0a4a413df7237982a7a414fa18e45bcad3571a
MrX-OvO <1176471624@qq.com>
2022-02-11 21:06:46 +0800

    second version minigrep, reconstitution part 1

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,16 +1,33 @@
-use std::env;
-use std::fs;
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("Search for {}", query);
-    println!("In file {}", filename);
-
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    let contents =
+        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
 }

aa0af9a844327fe1c5d1e091131c894e0cd25897
MrX-OvO <1176471624@qq.com>
2022-05-11 21:45:07 +0800

    rust oop simple test, add pub

--- oop/src/main.rs
+++ oop/src/main.rs
@@ -5,19 +5,27 @@ pub struct AveragedCollection {
 }
 
 impl AveragedCollection {
-    fn new() -> AveragedCollection {
+    pub fn list(&self) -> &[i32] {
+        &self.list[..]
+    }
+
+    pub fn average(&self) -> f64 {
+        self.average
+    }
+
+    pub fn new() -> AveragedCollection {
         AveragedCollection {
             list: vec![],
             average: 0.0,
         }
     }
 
-    fn add(&mut self, value: i32) {
+    pub fn add(&mut self, value: i32) {
         self.list.push(value);
         self.update_average();
     }
 
-    fn remove(&mut self) -> Option<i32> {
+    pub fn remove(&mut self) -> Option<i32> {
         let result = self.list.pop();
         if let Some(value) = result {
             self.update_average();
@@ -31,14 +39,6 @@ impl AveragedCollection {
         let total: i32 = self.list.iter().sum();
         let len = self.list.len();
         self.average = total as f64 / len as f64;
-    }
-
-    fn average(&self) -> f64 {
-        self.average
-    }
-
-    fn list(&self) -> &[i32] {
-        &self.list[..]
     }
 }
 

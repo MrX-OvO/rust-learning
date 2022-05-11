86e28ea8cb28947122899d48953718e3684750b1
MrX-OvO <1176471624@qq.com>
2022-05-11 20:30:48 +0800

    multi-thread message passing

--- multhread/src/main.rs
+++ multhread/src/main.rs
@@ -1,3 +1,4 @@
+use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
 
@@ -22,4 +23,53 @@ fn main() {
     });
 
     handle.join().unwrap();
+
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        let val = String::from("Hi");
+        tx.send(val).unwrap();
+        //println!("val is {}", val); // borrow of moved value: `val` value borrowed here after move
+    });
+    // recv()会阻塞主线程直到从信道中接收一个值
+    // try_recv()不会阻塞主线程，相反会立即返回一个Result<T, E>，
+    // Ok值包含有用的值，Err值表示此时没有任何消息
+    let recieved = rx.recv().unwrap();
+    println!("Got: {}", recieved);
+
+    {
+        let (tx, rx) = mpsc::channel();
+
+        let tx1 = tx.clone();
+        thread::spawn(move || {
+            let vals = vec![
+                String::from("hi"),
+                String::from("from"),
+                String::from("the"),
+                String::from("thread"),
+            ];
+
+            for val in vals {
+                tx1.send(val).unwrap();
+                thread::sleep(Duration::from_secs(1));
+            }
+        });
+
+        thread::spawn(move || {
+            let vals = vec![
+                String::from("more"),
+                String::from("messages"),
+                String::from("for"),
+                String::from("you"),
+            ];
+
+            for val in vals {
+                tx.send(val).unwrap();
+                thread::sleep(Duration::from_secs(1));
+            }
+        });
+
+        for received in rx {
+            println!("Got: {}", received);
+        }
+    }
 }

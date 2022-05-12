cb92266d6a899007eaa08f7e59da85eba81f04c0
MrX-OvO <1176471624@qq.com>
2022-05-12 21:23:40 +0800

    use TcpListener, add handle_connection, read stream to buffer, and print

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,10 +1,17 @@
-use std::net::TcpListener;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        println!("Connection established!")
+        handle_connection(stream);
     }
 }

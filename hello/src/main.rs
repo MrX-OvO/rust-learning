81e8d22dc0ac2db69680f071b590f45628b4fb6f
MrX-OvO <1176471624@qq.com>
2022-05-12 21:26:35 +0800

    use TcpListener, add response in handle_connection rather than print stream

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -4,7 +4,11 @@ use std::net::{TcpListener, TcpStream};
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
 
 fn main() {

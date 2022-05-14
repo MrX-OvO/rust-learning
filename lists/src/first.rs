34e1e7703e8b1fc6fffb79ea3f61d6b8144c694c
MrX-OvO <1176471624@qq.com>
2022-05-14 20:01:06 +0800

    implement lists first version

--- lists/src/first.rs
+++ lists/src/first.rs
@@ -39,6 +39,16 @@ impl List {
     }
 }
 
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
+
+        while let Link::More(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::List;
